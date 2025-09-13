7b7cdfc7a903510ad57263fb2f5bd9484052b3a5
Daniel Mihai <danielmihai@Daniels-Laptop.powerhub>
2025-09-13 12:53:17 +0100

    chore: sync 2025-09-13 12:53:17

--- src/routes/budgets.ts
+++ src/routes/budgets.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { body } from 'express-validator';
 import { requireAuth } from '../middleware/auth.js';
 import { handleValidation } from '../middleware/validate.js';
 import { deleteBudget, getBudgetUsage, listBudgets, setBudget, updateBudget } from '../controllers/budgetController.js';
